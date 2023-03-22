374c531bbe7af7492f3b8d8658fa12e386007132
bimogempar <bimogempar@gmail.com>
2023-03-22 21:48:29 +0700

    update: done integrate root page

--- frontend/src/pages/root.jsx
+++ frontend/src/pages/root.jsx
@@ -1,42 +1,67 @@
-import { Col, Row, Typography, Tabs } from 'antd';
+import { Col, Row, Typography, Tabs, Grid, message } from 'antd';
 import React, { useEffect, useState } from 'react';
 import TablePosts from '../components/TablePosts';
 import { ColumnTablePostData } from '../settings/ColumnTablePostData';
 import { OptionTab } from '../settings/OptionTab';
+import axios from 'axios';
 const { Title } = Typography;
+const { useBreakpoint } = Grid;
 
 const Root = () => {
-    const [filter, setFilter] = useState('');
+    const screens = useBreakpoint();
+    const [messageApi, contextHolder] = message.useMessage();
+    const [filter, setFilter] = useState('publish');
     const [data, setData] = useState([]);
 
     const fetchTable = () => {
-        console.log('fetch again')
-        setData([
-            {
-                title: 'title 1',
-                category: 'category 1',
-            },
-            {
-                title: 'title 2',
-                category: 'category 2',
-            }
-        ])
+        axios.get(`${process.env.REACT_APP_BE_URL}article`)
+            .then(res => {
+                const { data: { data: newData, message } } = res
+                if (newData.length > 0) {
+                    switch (filter) {
+                        case 'publish':
+                            setData(newData.filter(x => x.status === 'publish'))
+                            break;
+                        case 'draft':
+                            setData(newData.filter(x => x.status === 'draft'))
+                            break;
+                        case 'trash':
+                            setData(newData.filter(x => x.status === 'trash'))
+                            break;
+                        default:
+                            break;
+                    };
+                } else {
+                    setData([]);
+                }
+                messageApi.open({
+                    type: 'success',
+                    content: message,
+                })
+            }).catch(err => {
+                const { err: { message } } = err;
+                messageApi.open({
+                    type: 'error',
+                    content: message,
+                })
+            })
     }
 
     useEffect(() => {
         fetchTable();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [filter])
-
-    console.log(data);
 
     return (
         <Row
-            style={{ padding: '25px' }}
+            style={{ padding: screens.lg ? '15px' : 0 }}
             justify="center"
         >
+            {contextHolder}
             <Col
                 span={24}
-                lg={16}
+                lg={20}
+                xl={16}
                 style={{ padding: '15px' }}
             >
                 <Title level={2}>All Posts</Title>
--- frontend/src/settings/ColumnTablePostData.js
+++ frontend/src/settings/ColumnTablePostData.js
@@ -1,5 +1,5 @@
 import { DeleteTwoTone, EditTwoTone } from "@ant-design/icons";
-import { Col, Row } from "antd";
+import { Col, Row, Tag } from "antd";
 
 export const ColumnTablePostData = [
     {
@@ -11,6 +11,32 @@ export const ColumnTablePostData = [
         title: 'Category',
         dataIndex: 'category',
         key: 'category',
+    },
+    {
+        title: 'Status',
+        key: 'status',
+        render: record => {
+            const { status } = record;
+            let color
+            switch (status) {
+                case 'publish':
+                    color = 'green';
+                    break;
+                case 'draft':
+                    color = 'gold';
+                    break;
+                case 'trash':
+                    color = 'red';
+                    break;
+                default:
+                    break;
+            }
+            return (
+                <Tag color={color}>
+                    {status.toUpperCase()}
+                </Tag>
+            )
+        }
     },
     {
         title: 'Action',
