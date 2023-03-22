89cd553f4eb1da4b6d0c2f2b0d57eaee7a7d22bb
bimogempar <bimogempar@gmail.com>
2023-03-22 17:06:45 +0700

    refactor: remove mui and use antd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
+import Root from './pages/root';
+import 'antd/dist/reset.css';
 
 function App() {
   return (
     <Routes>
       <Route path="/">
-        <Route index element={<div>this index element comp</div>} />
+        <Route index element={<Root />} />
         <Route path="about" element={<div>this about element comp</div>} />
         <Route path="*" element={<div>not found page</div>} />
       </Route>
